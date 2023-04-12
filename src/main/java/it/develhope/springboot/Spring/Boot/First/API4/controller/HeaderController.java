f1b0cddb42a5467d7e6c27461e2e06da64b91a83
Alessio Limina <alessio.limina@gmail.com>
2023-04-12 20:13:54 +0200

    Exercise Spring Boot - First API - 04 - updated and fixed. Also added Postman Documentation and file JSON

--- src/main/java/it/develhope/springboot/Spring/Boot/First/API4/controller/HeaderController.java
+++ src/main/java/it/develhope/springboot/Spring/Boot/First/API4/controller/HeaderController.java
@@ -3,12 +3,15 @@ package it.develhope.springboot.Spring.Boot.First.API4.controller;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.InetSocketAddress;
+
 @RestController
 @RequestMapping("/api")
 public class HeaderController {
 
-    @GetMapping
-    public String getNameHost (@RequestParam(required = true) String hostName, @RequestParam (required = true) String hostPort, @RequestHeader HttpHeaders headers){
-        return "Hostname: " + headers.get(hostName) + ", host port: " + headers.get(hostPort);
+    @GetMapping("/")
+    public InetSocketAddress getNameHost (@RequestHeader HttpHeaders headers){
+        return headers.getHost();
     }
+    // Documentazione Postman: https://documenter.getpostman.com/view/26121086/2s93XvWjhi
 }
